90ae52197ecb99bca9b88d74ea4eb03d39c84394
Thomas Villaverde <thomas.villaverde@globant.com>
2019-04-29 12:59:07 -0300

    testing this

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hola soy Thomas Villaverde</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>probando esto </p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
